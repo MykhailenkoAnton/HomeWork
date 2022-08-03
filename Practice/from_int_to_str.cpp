18f7aa29357b54a17533642d65cefdec92499e21
MykhailenkoAnton <mykhailencko2594@gmail.com>
2022-08-03 19:26:06 +0300

    convert

---
+++ Practice/from_int_to_str.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+std::string FromStrToInt(int num)
+{
+    std::string result;
+    for (int i = num; i >= 1; i /= 10)
+    {
+        char temo = (i % 10) + '0';
+        result += temo;
+    }
+    return result;
+}
+
+int main()
+{
+    std::cout << "Enter a str numbers: ";
+    int num;
+    std::cin >> num;
+
+    std::string result = FromStrToInt(num);
+    std::cout << result << std::endl;
+}
