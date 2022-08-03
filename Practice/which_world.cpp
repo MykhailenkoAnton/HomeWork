257981776ba888c86442361d2ce1976f7c1d2f9f
MykhailenkoAnton <mykhailencko2594@gmail.com>
2022-08-03 12:30:44 +0300

    words

--- Practice/which_world.cpp
+++ Practice/which_world.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 #include <string>
-
+#include <cstring>
+#include <cctype>
+int wordsCount(const std::string & sentence)
+{
+    int count = 0;
+    for (int i = 0; i < sentence.size(); i++)
+    {
+        if (isalpha(sentence[i]))
+        {
+            while (sentence[i] != ' ' && i < sentence.size())
+            {
+                i++;
+                continue;
+            }
+            count++;
+        }
+    }
+    return count;
+}
 
 int main()
 {
-    std::cout << 1 << std::endl;
+    std::string words;
+    std::cout << "Enter a sentence please: ";
+    std::getline(std::cin, words);
+
+    int result = wordsCount(words);
+    std::cout << "This sentence has " << result << " words\n";
+
     return 0;
 }
